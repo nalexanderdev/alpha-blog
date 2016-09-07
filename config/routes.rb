a73d5a8890a6dc66eab850b6076abcf2d304c724
Nick Udemy Course <nalexanderdev@gmail.com>
2016-09-07 11:55:12 +0000

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ get 'about', to: 'pages#about'
 
 resources :articles
 
+get 'signup', to: 'users#new'
+resources :users, except: [:new]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
